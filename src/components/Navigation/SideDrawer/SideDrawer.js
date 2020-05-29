b3e1c11f3ea99219666062ec8ec8d8692319fceb
aditya13jani <44555223+aditya13jani@users.noreply.github.com>
2020-05-29 19:59:48 -0400

    finally!!

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationIterms/NavigationItems';
 import classes from './SideDrawer.module.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/AuxComp';
 import BackDrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
